ae792c406d0c874037ba630e18d59a8aa627aa54
William <willmclaughlin13@gmail.com>
2019-09-28 14:32:55 -0700

    Listed all symbols.

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -8,7 +8,12 @@
 
 using namespace std;
 
-static string token_names[] = {	"EOF_T" };
+static string token_names[] = {	"IDENT_T", "NUMLIT_T", "STRLIT_T", "LISTOP_T", "CONS_T", "IF_T",
+                                "COND_T", "ELSE_T", "DISPLAY_T", "NEWLINE_T", "AND_T", "OR_T",
+                                "NOT_T", "DEFINE_T", "NUMBERP_T", "LISTP_T", "ZEROP_T", "NULLP_T",
+                                "STRINGP_T", "PLUS_T", "MINUS_T", "DIV_T", "MULT_T", "MODULO_T",
+                                "ROUND_T", "EQUALTO_T", "GT_T", "LT_T", "GTE_T", "LTE_T", "LPAREN_T",
+                                "RPAREN_T", "SQUOTE_T", "ERROR_T", "EOF_T"};
 
 LexicalAnalyzer::LexicalAnalyzer (char * filename)
 {
