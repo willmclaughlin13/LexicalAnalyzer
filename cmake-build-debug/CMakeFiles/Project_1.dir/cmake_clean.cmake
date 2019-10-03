8dc0732e08ab5d511916f45e168d31ac04744031
William <willmclaughlin13@gmail.com>
2019-10-03 15:32:52 -0700

    Added Cmake

---
+++ cmake-build-debug/CMakeFiles/Project_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_1.dir/Project1.cpp.o"
+  "CMakeFiles/Project_1.dir/LexicalAnalyzer.cpp.o"
+  "CMakeFiles/Project_1.dir/SetLimits.cpp.o"
+  "CMakeFiles/Project_1.dir/SyntacticalAnalyzer.cpp.o"
+  "Project_1.pdb"
+  "Project_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
